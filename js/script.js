1f78627a320542a2e6ac243f92428c4d3a925731
George Hlongwane <55334979+V-FOR-VEND3TTA@users.noreply.github.com>
2021-06-24 03:50:29 +0200

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function show() {
+  document.getElementById("sidebar").classList.toggle("active");
+}
+/*
+const menuToggle = document.querySelector(".toggle");
+const showcase = document.querySelector(".showcase");
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  showcase.classList.toggle("active");
+});*/
